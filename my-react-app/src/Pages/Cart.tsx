79e68755fb63c8b91f00f41c766638f6c4666585
Raphael Bajet <baphael@gmail.com>
2023-12-04 10:39:37 -0800

    made Menu look better

--- my-react-app/src/Pages/Cart.tsx
+++ my-react-app/src/Pages/Cart.tsx
@@ -3,23 +3,42 @@ import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header';
 import ItemInCart from '../components/ItemInCart';
+import ChangeQuantity from '../Components/ChangeQuantity';
 
-function Cart({ cart }) {
+function Cart({ cart, countDecimalDigits }) {
     const navigate = useNavigate();
+
+    const borderStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        border: '0.35vw solid var(--red40)',
+        borderRadius: '3vw',
+        marginLeft: '10vw',
+        marginRight: '10vw',
+        marginBottom: '10vw',
+        padding: '2vw',
+        width: '80vw',
+    };
+
     return (
         <>
             <Header />
-            CART
-            {cart.map((cartItem) => (
-                <ItemInCart
-                    key={cartItem.cartNumber}
-                    cart={cart}
-                    cartNumber={cartItem.cartNumber}
-                />
-            ))}
-            <button onClick={() => navigate('/time-and-date')}>
-                Proceed to Time and Date
-            </button>
+            <div className="page" style={{ marginTop: '3vw' }}>
+                <div className="heading-0">Cart</div>
+                <div style={borderStyle}>
+                    {cart.map((cartItem) => (
+                        <ItemInCart
+                            key={cartItem.cartNumber}
+                            cart={cart}
+                            cartNumber={cartItem.cartNumber}
+                            countDecimalDigits={countDecimalDigits}
+                        />
+                    ))}
+                </div>
+                <button onClick={() => navigate('/time-and-date')}>
+                    Proceed to Time and Date
+                </button>
+            </div>
         </>
     );
 }
--- my-react-app/src/Pages/MenuItem.jsx
+++ my-react-app/src/Pages/MenuItem.jsx
@@ -3,30 +3,21 @@ import { useNavigate } from 'react-router-dom';
 
 import Header from '../Components/Header';
 import GoToCart from '../Components/GoToCart';
+import ChangeQuantity from '../Components/ChangeQuantity';
 
-function MenuItem({ addToCart, cartItem, quantity, setQuantity, cartNumber }) {
+function MenuItem({
+    addToCart,
+    cartItem,
+    quantity,
+    setQuantity,
+    cartNumber,
+    countDecimalDigits,
+}) {
     const navigate = useNavigate();
-
-    function countDecimalDigits(number) {
-        const decimalPart = number.toString().split('.')[1];
-        return decimalPart.length;
-    }
 
     const handleClick = () => {
         addToCart(cartItem);
         navigate('/');
-    };
-
-    const increase = () => {
-        if (quantity < 10) {
-            setQuantity(quantity + 1);
-        }
-    };
-
-    const decrease = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
-        }
     };
 
     const imageStyle = {
@@ -50,6 +41,7 @@ function MenuItem({ addToCart, cartItem, quantity, setQuantity, cartNumber }) {
         alignItems: 'center',
         width: '50vw',
         margin: '0',
+        objectFit: 'cover',
     };
 
     const lineStyle = {
@@ -73,35 +65,6 @@ function MenuItem({ addToCart, cartItem, quantity, setQuantity, cartNumber }) {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-    };
-
-    const signStyle = {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-    };
-
-    const plusStyle = {
-        fontSize: '5vw',
-        borderRadius: '100vw 0 0 100vw',
-        border: 'none',
-        borderRight: '0.25vw solid var(--red100)',
-        color: 'var(--white)',
-    };
-
-    const minusStyle = {
-        fontSize: '5vw',
-        borderRadius: '0 100vw 100vw 0',
-        border: 'none',
-        borderLeft: '0.25vw solid var(--red100)',
-        color: 'var(--white)',
-    };
-
-    const boldStyle = {
-        fontFamily: 'Sansation-bold',
-        margin: '0',
-        padding: '0',
-        transform: 'translate(0, -1.65vw)',
     };
 
     const addtoCartStyle = {
@@ -152,22 +115,10 @@ function MenuItem({ addToCart, cartItem, quantity, setQuantity, cartNumber }) {
                             <p className="heading-2" style={{ width: '20vw' }}>
                                 quantity: {cartItem.quantity}
                             </p>
-                            <div style={signStyle}>
-                                <button
-                                    className="heading-2 sign"
-                                    onClick={increase}
-                                    style={plusStyle}
-                                >
-                                    <p style={boldStyle}>+</p>
-                                </button>
-                                <button
-                                    onClick={decrease}
-                                    className="heading-2 sign"
-                                    style={minusStyle}
-                                >
-                                    <p style={boldStyle}>-</p>
-                                </button>
-                            </div>
+                            <ChangeQuantity
+                                quantity={quantity}
+                                setQuantity={setQuantity}
+                            />
                             <div
                                 style={{
                                     display: 'flex',
